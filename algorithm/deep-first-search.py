6f6d3f0fa088e151974653941e376bd3b807c08e
garyjeong <smat91@naver.com>
2024-05-18 23:21:36 +0900

    fix: dfs algorithm

--- algorithm/deep-first-search.py
+++ algorithm/deep-first-search.py
@@ -2,21 +2,7 @@
 # 기본적으로 스택 사용, 재귀 함수를 이용하여 암묵적으로도 사용
 # 모든 노드에 대하여 검색하고 방문하되 시작되면 끝 점까지 탐색
 # base_image.jpeg를 기준으로 탐색
-
-graph: list = {
-    "A": ["B", "C"],
-    "B": ["E", "F"],
-    "C": ["D", "E"],
-    "D": ["H"],
-    "E": ["G"],
-    "F": ["I"],
-    "G": ["H", "J"],
-    "H": ["L"],
-    "I": ["J"],
-    "J": ["K"],
-    "K": ["L"],
-    "L": [],
-}
+# 깊이 우선 탐색은 최단거리를 보장하지 않는다
 
 
 def deep_first_search(
@@ -47,6 +33,20 @@ def deep_first_search(
                 return result
 
 
+graph: list = {
+    "A": ["B", "C"],
+    "B": ["E", "F"],
+    "C": ["D", "E"],
+    "D": ["H"],
+    "E": ["G", "H"],
+    "F": ["I", "G"],
+    "G": ["J", "L"],
+    "H": ["L"],
+    "I": ["J", "K"],
+    "J": ["K"],
+    "K": ["L"],
+    "L": [],
+}
 # 시작 노드 A에서 종료 노드 L까지의 경로
 result = deep_first_search(graph=graph, start="A", end="L")
 print("경로 : ", result)
