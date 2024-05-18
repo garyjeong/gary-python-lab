6258623e3ef3b7843e0dcf922654e25ef076f93b
garyjeong <smat91@naver.com>
2024-05-18 19:21:30 +0900

    fix: add deep first search algorithm

---
+++ algorithm/deep-first-search.py
@@ -0,0 +1,52 @@
+# 깊이 우선 탐색, DFS
+# 기본적으로 스택 사용, 재귀 함수를 이용하여 암묵적으로도 사용
+# 모든 노드에 대하여 검색하고 방문하되 시작되면 끝 점까지 탐색
+# base_image.jpeg를 기준으로 탐색
+
+graph: list = {
+    "A": ["B", "C"],
+    "B": ["E", "F"],
+    "C": ["D", "E"],
+    "D": ["H"],
+    "E": ["G"],
+    "F": ["I"],
+    "G": ["H", "J"],
+    "H": ["L"],
+    "I": ["J"],
+    "J": ["K"],
+    "K": ["L"],
+    "L": [],
+}
+
+
+def deep_first_search(
+    graph: list,
+    start: str,
+    end: str,
+    path: list | None = None,
+    visited: bool | None = None,
+):
+    if not visited or not isinstance(visited, set):
+        visited = set()
+
+    if not path:
+        path = []
+
+    visited.add(start)
+    path.append(start)
+
+    if start == end:
+        return path
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            result = deep_first_search(
+                graph=graph, start=neighbor, end=end, path=path, visited=visited
+            )
+            if result is not None:
+                return result
+
+
+# 시작 노드 A에서 종료 노드 L까지의 경로
+result = deep_first_search(graph=graph, start="A", end="L")
+print("경로 : ", result)
